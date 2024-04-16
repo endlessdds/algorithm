1c4c5c652a741736f0c2ba00a3dc9406236a9c23
dds <1934423564@qq.com>
2024-04-16 22:16:23 +0800

    google style

--- include/dsu.h
+++ include/dsu.h
@@ -6,34 +6,34 @@
 
 class dsu {
 public:
-  dsu(int n) : f_{std::vector<int>(n)}, s_{std::vector<int>(n, 1)} {
-    for (int i = 0; i < n; i++) {
-      f_[i] = i;
+    dsu(int n) : f_{std::vector<int>(n)}, s_{std::vector<int>(n, 1)} {
+        for (int i = 0; i < n; i++) {
+            f_[i] = i;
+        }
     }
-  }
 
-  int find(int x) {
-    if (f_[x] == x) {
-      return x;
+    int find(int x) {
+        if (f_[x] == x) {
+            return x;
+        }
+        return f_[x] = find(f_[x]);
     }
-    return f_[x] = find(f_[x]);
-  }
 
-  void merge(int a, int b) {
-    int x = find(a), y = find(b);
-    if (x == y) {
-      return;
+    void merge(int a, int b) {
+        int x = find(a), y = find(b);
+        if (x == y) {
+            return;
+        }
+        if (s_[x] < s_[y]) {
+            std::swap(x, y);
+        }
+        f_[y] = x;
+        s_[x] += f_[y];
     }
-    if (s_[x] < s_[y]) {
-      std::swap(x, y);
-    }
-    f_[y] = x;
-    s_[x] += f_[y];
-  }
 
 private:
-  std::vector<int> f_;
-  std::vector<int> s_;
+    std::vector<int> f_;
+    std::vector<int> s_;
 };
 
-#endif // !DEBUG
+#endif  // !DEBUG
--- include/odt.h
+++ include/odt.h
@@ -6,59 +6,59 @@
 #include <set>
 
 class odt {
-  using ll = long long;
+    using ll = long long;
 
 public:
-  odt(ll l, ll r, ll v) : val(std::set<node>{node{l, r, v}}) {}
+    odt(ll l, ll r, ll v) : val(std::set<node>{node{l, r, v}}) {}
 
-  void assign(ll l, ll r, ll v) {
-    auto it1 = split(l);
-    auto it2 = split(r + 1);
-    val.erase(it1, it2);
-    val.insert(node{l, r, v});
-  }
-
-  ll sum(ll l, ll r) {
-    ll sum = 0;
-    auto it1 = split(l);
-    auto it2 = split(r + 1);
-    for (; it1 != it2; ++it1) {
-      sum += it1->v * (it1->r - it1->l + 1);
+    void assign(ll l, ll r, ll v) {
+        auto it1 = split(l);
+        auto it2 = split(r + 1);
+        val.erase(it1, it2);
+        val.insert(node{l, r, v});
     }
-    return sum;
-  }
 
-  ll max(ll l, ll r) {
-    ll max = INT64_MIN;
-    auto it1 = split(l);
-    auto it2 = split(r + 1);
-    for (; it1 != it2; ++it1) {
-      max = std::max(max, it1->v);
+    ll sum(ll l, ll r) {
+        ll sum = 0;
+        auto it1 = split(l);
+        auto it2 = split(r + 1);
+        for (; it1 != it2; ++it1) {
+            sum += it1->v * (it1->r - it1->l + 1);
+        }
+        return sum;
     }
-    return max;
-  }
+
+    ll max(ll l, ll r) {
+        ll max = INT64_MIN;
+        auto it1 = split(l);
+        auto it2 = split(r + 1);
+        for (; it1 != it2; ++it1) {
+            max = std::max(max, it1->v);
+        }
+        return max;
+    }
 
 private:
-  struct node {
-    ll l, r, v;
-    bool operator<(const node &rhs) const { return l < rhs.l; }
-  };
+    struct node {
+        ll l, r, v;
+        bool operator<(const node &rhs) const { return l < rhs.l; }
+    };
 
-  std::set<node> val;
+    std::set<node> val;
 
-  std::set<node>::iterator split(ll x) {
-    auto it = --val.upper_bound(node{x, 0, 0});
-    if (it->l == x) {
-      return it;
+    std::set<node>::iterator split(ll x) {
+        auto it = --val.upper_bound(node{x, 0, 0});
+        if (it->l == x) {
+            return it;
+        }
+        auto [l, r, v] = *it;
+        if (x > r) {
+            return val.end();
+        }
+        val.erase(it);
+        val.insert(node{l, x - 1, v});
+        return val.insert(node{x, r, v}).first;
     }
-    auto [l, r, v] = *it;
-    if (x > r) {
-      return val.end();
-    }
-    val.erase(it);
-    val.insert(node{l, x - 1, v});
-    return val.insert(node{x, r, v}).first;
-  }
 };
 
 #endif
